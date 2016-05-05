fbc1b5c5cef5424ee21aa4201427cfeae4e13b61
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-05-05 17:55:07 +0900

    feat(days/day-1/exercises): improve examples

--- days/day-1/src/exercises/test/main.js
+++ days/day-1/src/exercises/test/main.js
@@ -2,5 +2,11 @@
   "use strict";
   var assert = namespace.TestUtils.assert;
 
-  assert.equal(1, 1);
+  console.log("成功例 ===========");
+  assert.ok(1 + 1 === 2, "例1");
+  assert.fail(1 + 1 !== 2, "例2");
+
+  console.log("失敗 ===========");
+  assert.ok(1 + 1 === 3, "例3");
+  assert.fail(1 + 1 !== 3, "例4");
 })(window.JavaScriptWorkshop);
