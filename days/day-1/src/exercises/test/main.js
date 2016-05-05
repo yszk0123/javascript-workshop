8327980ded9c827473aab4899ce3939791ffddc7
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-05-05 18:49:08 +0900

    feat(days/day-1/exercises): pretty print

--- days/day-1/src/exercises/test/main.js
+++ days/day-1/src/exercises/test/main.js
@@ -2,11 +2,13 @@
   "use strict";
   var assert = namespace.TestUtils.assert;
 
-  console.log("成功例 ===========");
+  console.group("========== 成功例 ==========");
   assert.ok(1 + 1 === 2, "例1");
   assert.fail(1 + 1 !== 2, "例2");
+  console.groupEnd();
 
-  console.log("失敗 ===========");
+  console.group("========== 失敗 ==========");
   assert.ok(1 + 1 === 3, "例3");
   assert.fail(1 + 1 !== 3, "例4");
+  console.groupEnd();
 })(window.JavaScriptWorkshop);
