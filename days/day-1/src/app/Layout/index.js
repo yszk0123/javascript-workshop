eb11d9f44e629f8e9de68018072ab060375fb132
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-05-05 12:34:32 +0900

    refactor(days/day-1/app): Move Layout into Layout/

--- days/day-1/src/app/Layout/index.js
+++ days/day-1/src/app/Layout/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-
-import { exerciseRoutes, documentRoutes } from "./routes";
-import ExerciseLinks from "./components/ExerciseLinks";
-import DocumentLinks from "./components/DocumentLinks";
-import { Outer } from "./undefined-components";
-
 import "normalize.css";
+
+import { exerciseRoutes, documentRoutes } from "../routes";
+import ExerciseLinks from "../components/ExerciseLinks";
+import DocumentLinks from "../components/DocumentLinks";
+import { Outer } from "../undefined-components";
+
 import * as Styles from "./styles.css";
 
 const LinkGroup = (props) =>
@@ -14,8 +14,14 @@ const LinkGroup = (props) =>
 const MainContent = (props) =>
   <div className={Styles.MainContent} {...props} />;
 
-const LinkGroupLayout = () => (
-  <div className={Styles.LinkGroupLayout}>
+const LeftPane = (props) =>
+  <div className={Styles.LeftPane} {...props} />;
+
+const RightPane = (props) =>
+  <div className={Styles.RightPane} {...props} />;
+
+const LinkGroupOuter = () => (
+  <div className={Styles.LinkGroupOuter}>
     {!!exerciseRoutes.length &&
       <LinkGroup>
         <ExerciseLinks routes={exerciseRoutes} />
@@ -29,16 +35,10 @@ const LinkGroupLayout = () => (
   </div>
 );
 
-const LeftPane = (props) =>
-  <div className={Styles.LeftPane} {...props} />;
-
-const RightPane = (props) =>
-  <div className={Styles.RightPane} {...props} />;
-
 const Layout = ({ children }) => (
   <Outer>
     <LeftPane>
-      <LinkGroupLayout />
+      <LinkGroupOuter />
     </LeftPane>
     <RightPane>
       <MainContent>
