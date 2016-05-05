2417e05a6fce253298c24b2ceb2621a8b99eb7bc
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-05-05 19:55:20 +0900

    feat(days/day-1/exercises): add an exercise "function"

---
+++ days/day-1/src/exercises/function/main.js
@@ -0,0 +1,57 @@
+// 演習: ここで "use strict"; を使うとどうなるか
+
+(function(namespace) {
+  "use strict";
+  var assert = namespace.TestUtils.assert;
+
+  // 普通の関数を宣言して呼び出す場合 (関数宣言)
+
+  function iife() {
+    console.log("IIFE 1");
+  }
+
+  iife();
+
+  // 関数式は値(関数は値!)を返す
+
+  var iife2 = function() {
+    console.log("IIFE 2");
+  };
+
+  iife2();
+
+  // 宣言してそのまま使う (関数式)
+
+  (function iife3() {
+    console.log("IIFE 3");
+  })();
+
+  // 名前をつけない (無名関数、これも関数式)
+
+  (function() {
+    console.log("IIFE 4");
+  })();
+
+  // 比較
+
+  (iife)();
+  (function() { console.log("IIFE 5"); })();
+
+  // ただの関数なので普通に引数を与えることもできる
+
+  var iife6 = function(global) {
+    console.log("IIFE 6: global = window =", global);
+  };
+  iief6(window);
+
+  (function(global) {
+    console.log("IIFE 7: global = window =", global);
+  })(window);
+
+  // ちょっと別の書き方
+
+  (function() { console.log("IIFE 8"); }());
+
+  // 演習: なぜ括弧が必要か
+  assert.ok(false, "演習");
+})(window.JavaScriptWorkshop);
