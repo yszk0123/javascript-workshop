6aded3a538c08a1b3b012323cf61c4cbb36b3132
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-05-07 05:09:01 +0900

    feat(days/day-1/app): use reselect

--- days/day-1/src/app/selectors.js
+++ days/day-1/src/app/selectors.js
@@ -1,26 +1,37 @@
+import { createSelector } from "reselect";
+
 import ContentType from "./ContentType";
 
 // TODO: Don't use Array#find()
-export const currentContentSelector = ({ contents }, { location }) =>
-  contents.find((content) => content.absolutePath === location.pathname);
+export const currentContentSelector = createSelector(
+  [
+    (state) => state.contents,
+    (state, ownProps) => ownProps.location
+  ],
+  (contents, location) =>
+    contents.find((content) => content.absolutePath === location.pathname)
+);
 
-export const contentsGroupsSelector = ({ contents }) => {
-  const groupsByType = contents.reduce((acc, content) => {
-    const item = acc[content.type] = acc[content.type] || [];
-    item.push(content);
-    return acc;
-  }, {});
+export const contentsGroupsSelector = createSelector(
+  (state) => state.contents,
+  (contents) => {
+    const groupsByType = contents.reduce((acc, content) => {
+      const item = acc[content.type] = acc[content.type] || [];
+      item.push(content);
+      return acc;
+    }, {});
 
-  return [
-    {
-      type: ContentType.Doc,
-      path: "docs",
-      contents: groupsByType[ContentType.Doc]
-    },
-    {
-      type: ContentType.Exercise,
-      path: "exercises",
-      contents: groupsByType[ContentType.Exercise]
-    }
-  ].filter(Boolean);
-};
+    return [
+      {
+        type: ContentType.Doc,
+        path: "docs",
+        contents: groupsByType[ContentType.Doc]
+      },
+      {
+        type: ContentType.Exercise,
+        path: "exercises",
+        contents: groupsByType[ContentType.Exercise]
+      }
+    ].filter(Boolean);
+  }
+);
