9d50ae875719b22094378dee8b6137bce9685b75
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-05-14 20:51:27 +0900

    refactor(days/day-1): improve comments

--- days/day-1/src/exercises/070-click-counter-1/better.js
+++ days/day-1/src/exercises/070-click-counter-1/better.js
@@ -16,29 +16,24 @@
   // アプリの見た目を扱う
   function createView(model) {
     // DOM要素にアクセスするための変数を宣言
-
     const incrementButtonElement = document.getElementById('increment-button');
     const counterElement = document.getElementById('counter');
 
     // クリックされた時の動作を定義
-
     function onIncrementButtonClick(_event) {
       model.increment();
       updateCounter();
     }
 
     // 画面更新の定義
-
     function updateCounter() {
       counterElement.innerText = model.count;
     }
 
     // DOM要素のイベントとクリックされた時の動作を結びつける
-
     incrementButtonElement.addEventListener('click', onIncrementButtonClick);
 
-    // 画面更新
-
+    // createView の呼び出し元から updateCounter を参照できるようにする
     return {
       updateCounter: updateCounter
     };
