09d6accb1a5f7432471731401fdbb871b3a184b2
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-06-13 08:17:19 +0900

    feat(app): hide docs

--- src/app/reducers.js
+++ src/app/reducers.js
@@ -2,7 +2,7 @@ import assign from 'object-assign';
 import { SEARCH_TEXT_CHANGED, SEARCH_TAGS_CHANGED } from './constants';
 
 // TODO: tags の初期値を自動で推測する
-function searchReducer(state = { text: '', tags: ['doc', 'es5', 'es6'] }, action) {
+function searchReducer(state = { text: '', tags: ['es5', 'es6'] }, action) {
   switch (action.type) {
     case SEARCH_TEXT_CHANGED:
       return assign({}, state, {
