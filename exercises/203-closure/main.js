3b9180173c1e510596669ab1e9b82f4cf3e41c4a
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-06-28 05:17:49 +0900

    refactor(exercises): improve function names

--- exercises/203-closure/main.js
+++ exercises/203-closure/main.js
@@ -4,31 +4,31 @@ import { describe, assert } from 'jw-test-utils';
 // (main)-->(グローバルオブジェクト)
 export default function main() {
   // スコープチェーン:
-  // (makeGetValue)--> { assert: ..., makeGetValue: ..., getValue: ... } --(main)--> ...
-  function makeGetValue() {
+  // (makeCounter)--> { assert: ..., makeCounter: ..., count: ... } --(main)--> ...
+  function makeCounter() {
     const value = 1;
 
     // スコープチェーン:
-    // (getValueInner)--> { value: 1, getValueInner: ... } --(makeGetValue)--> ...
-    function getValueInner() {
+    // (countClosure)--> { value: 1, countClosure: ... } --(makeCounter)--> ...
+    function countClosure() {
       return value;
     }
 
-    return getValueInner;
+    return countClosure;
   }
 
-  // getValueInner() はクロージャなので、次のスコープチェーンを保持している
-  // (getValueInner)--> { value: 1, getValueInner: ... } --(makeGetValue)--> ...
-  // なので、makeGetValue() の外側にいるにもかかわらず、getValue() は value を参照できる
-  const getValue = makeGetValue();
+  // countClosure() はクロージャなので、次のスコープチェーンを保持している
+  // (countClosure)--> { value: 1, countClosure: ... } --(makeCounter)--> ...
+  // なので、makeCounter() の外側にいるにもかかわらず、count() は value を参照できる
+  const count = makeCounter();
 
-  describe('makeGetValue() の内側の変数を外から参照できる', function() {
-    assert.ok(getValue() === 1, '例1');
+  describe('makeCounter() の内側の変数を外から参照できる', function() {
+    assert.ok(count() === 1, '例1');
   });
 
-  describe('演習: makeGetValue() を修正して、getValue() を呼び出す度に value の値が繰り上がるようにする', function() {
-    assert.ok(getValue() === 2, '繰り上がる');
-    assert.ok(getValue() === 3, '繰り上がる');
-    assert.ok(getValue() === 4, '繰り上がる');
+  describe('演習: makeCounter() を修正して、count() を呼び出す度に value の値が繰り上がるようにする', function() {
+    assert.ok(count() === 2, '繰り上がる');
+    assert.ok(count() === 3, '繰り上がる');
+    assert.ok(count() === 4, '繰り上がる');
   });
 }
